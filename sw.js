34544af9a621f3574abdabb666ed389aa528c20a
Luis Cruz <76088698+Product-of-code@users.noreply.github.com>
2026-09-15 19:15:15 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'schedule-app-v1';
+const ASSETS_TO_CACHE = [
+  '/schedule-app/',
+  '/schedule-app/index.html',
+  '/schedule-app/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
